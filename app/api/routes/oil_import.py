c07d44129316347041d2e98aedbf72c5dd8da69b
Ajinkya <akale881@gmail.com>
2025-03-08 18:21:36 -0500

    refactoring

--- app/api/routes/oil_import.py
+++ app/api/routes/oil_import.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 from typing import Any
-
+import os
 
 router = APIRouter(prefix="/oil_price", tags=["oil"])
 
 @router.get("/")
-def read_items(
-    skip: int = 0, limit: int = 100
-) -> Any:
-    return
+def list_items() -> Any:
+    return os.environ.get("DATABASE_URL", None)
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from app.api.main import api_router
+import os
 
 app = FastAPI(
     title="crude oil service",
     openapi_url="/v1/openapi.json"
 )
 
+DATABASE_URL = os.environ.get("DATABASE_URL", None)
+print(f"printing db {DATABASE_URL}")
 app.include_router(api_router)
