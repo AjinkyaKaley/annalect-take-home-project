355ea2cac5dab4ebb3d4149e234298d5e5a81d55
Ajinkya <akale881@gmail.com>
2025-03-08 18:20:47 -0500

    volumns and init script setup

---
+++ init-scripts/02_create_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS commodity.oil(
+    year integer,
+    month integer,
+    originName text,
+    originTypeName text,
+    destinationName text,
+    destinationTypeName text,
+    gradeName text,
+    quantity integer
+)
