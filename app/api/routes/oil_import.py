e1ab3c20203d2a08b29384c3fc106fb61a5b4881
Ajinkya <akale881@gmail.com>
2025-03-09 16:33:45 -0400

    implemented insert functionality

--- app/api/routes/oil_import.py
+++ app/api/routes/oil_import.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel, Field
 from settings import settings
 from psycopg.rows import class_row
-from dataclasses import dataclass
+import dataclasses
 from typing import Any
 import psycopg
 import os
@@ -10,10 +11,7 @@ router = APIRouter(prefix="/oil", tags=["oil"])
 
 DB_CONNSTR=os.environ.get("DATABASE_URL", None)
 
-
-@dataclass
-class oil_data:
-    id: int
+class oil_data(BaseModel):
     year:int
     month: int
     originname: str
@@ -23,12 +21,15 @@ class oil_data:
     gradename: str
     quantity: int
 
+class oil_record(oil_data):
+    id: int
+
 def get_list(filter:str, page_num:int, page_limit:int, conn):
     column_name, value = filter.split("=")
     column_name = column_name.strip()
     value = value.strip()
 
-    with conn.cursor(row_factory=class_row(oil_data)) as cur:
+    with conn.cursor(row_factory=class_row(oil_record)) as cur:
         query = f"""
                     SELECT * 
                     FROM 
@@ -41,7 +42,7 @@ def get_list(filter:str, page_num:int, page_limit:int, conn):
         return cur.fetchall()
 
 def get_record(id, conn):
-    with conn.cursor(row_factory=class_row(oil_data)) as curr:
+    with conn.cursor(row_factory=class_row(oil_record)) as curr:
         query = f"""
                     SELECT *
                     FROM
@@ -50,13 +51,30 @@ def get_record(id, conn):
                 """
         curr.execute(query)
         return curr.fetchall()
-    
+
+def insert_record(data, conn)->int:
+    with conn.cursor() as cur:
+        columns = ",".join(list(data.model_dump().keys()))
+        values  = tuple(map(str,data.model_dump().values()))
+
+        query = f"""
+                    INSERT INTO commodity.oil ({columns})
+                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+                    RETURNING id
+                """
+        cur.execute(query, values)
+        conn.commit()
+
+        id = cur.fetchone()[0]
+        return id
+
+
 def get_dbconn():
     PG_USER = settings.PG_USER
     PG_PASSWORD = settings.PG_PASSWORD
     PG_DATABASE = settings.PG_DATABASE
     PG_HOSTNAME = settings.PG_HOSTNAME
-    
+
     if not PG_USER or not PG_PASSWORD or not PG_DATABASE:
         raise Exception("Invalid PG credentials, check .env file")
 
@@ -85,3 +103,10 @@ def get_record_by_id(
     conn=Depends(get_dbconn)
 )->Any:
     return get_record(id, conn)
+
+@router.put("/record")
+def add_new_record(
+    data: oil_data,
+    conn=Depends(get_dbconn)
+)->int:
+    return insert_record(data, conn)
